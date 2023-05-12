1d838ae76c3e9bcf39a101c559443b1d19f70ed0
Aman kumar <amankumar12091999@gmail.com>
2023-05-12 14:04:37 +0530

    style: added layout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,24 @@
+import "./Home.css";
 export const Home = () => {
-  return <>{/* <img src={require("../Images/banner_image_1.png")} /> */}</>;
+  return (
+    <>
+      <section className="category">
+        <h2>Categories</h2>
+        <ul>
+          <li>Mens</li>
+          <li>Women</li>
+          <li>Kids</li>
+          <li>accessories</li>
+        </ul>
+      </section>
+      <img src={require("../Images/3588601.jpg")} />
+      <section className="offers">
+        <h2>Offers</h2>
+        <ul>
+          <li>10%</li>
+          <li>20%</li>
+        </ul>
+      </section>
+    </>
+  );
 };
