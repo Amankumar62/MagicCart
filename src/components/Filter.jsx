0529eaef69c3025d725239b87dd7936c2ba83846
Aman kumar <amankumar12091999@gmail.com>
2023-05-14 01:39:41 +0530

    style: filter style added

---
+++ src/components/Filter.jsx
@@ -0,0 +1,54 @@
+import "./Filter.css";
+export const Filter = () => {
+  return (
+    <>
+      <form className="filter-form">
+        <header className="filter-header">
+          <h3>Filters</h3>
+          <button>Clear</button>
+        </header>
+        <h3>Price</h3>
+        <label className="range-label">
+          <span>0</span>
+          <span>1000</span>
+          <span>2000</span>
+        </label>
+        <input type="range" min="0" max="2000" list="range" id="price-range" />
+        <datalist id="range">
+          <option value="0"></option>
+          <option value="1000"></option>
+          <option value="2000"></option>
+        </datalist>
+        <h3>Category</h3>
+        <label>
+          <input type="checkbox" name="category" />
+          Men Clothing
+        </label>
+        <label>
+          <input type="checkbox" name="category" />
+          Women Clothing
+        </label>
+        <h3>Rating</h3>
+        <label>
+          <input type="radio" name="rating" /> 4 Stars & above
+        </label>
+        <label>
+          <input type="radio" name="rating" /> 3 Stars & above
+        </label>
+        <label>
+          <input type="radio" name="rating" /> 2 Stars & above
+        </label>
+        <label>
+          <input type="radio" name="rating" /> 1 Stars & above
+        </label>
+        <h3>Sort by</h3>
+        <label>
+          <input type="radio" name="sortby" /> Price-Low to High
+        </label>
+        <label>
+          <input type="radio" name="sortby" /> Price-Low to High
+        </label>
+      </form>
+    </>
+  );
+};
