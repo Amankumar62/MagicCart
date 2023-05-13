c4cf8f9b92e786706945b3be8acbbcfee00ca26b
Aman kumar <amankumar12091999@gmail.com>
2023-05-14 02:28:22 +0530

    style: Wishlist button added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import "./ProductCard.css";
+import { AiTwotoneHeart } from "react-icons/ai";
 export const ProductCard = () => {
   return (
     <>
@@ -7,6 +8,9 @@ export const ProductCard = () => {
         src="https://rukminim1.flixcart.com/image/612/612/k66sh3k0/jacket/a/k/u/m-9587613-roadster-original-imafzpbv8smzbquw.jpeg?q=70"
         alt="pro banner"
       />
+      <button className="btn-wishlist">
+        <AiTwotoneHeart className="btn-wishlist-icon" />
+      </button>
       <section className="product-detail">
         <span className="product-name">Men Premium Jacket</span>
         <span className="product-price">2000</span>
